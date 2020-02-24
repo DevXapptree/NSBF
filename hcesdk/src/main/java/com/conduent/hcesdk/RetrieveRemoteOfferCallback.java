1429b7a4cb19d57fce9ccf3bb30e4cb769740846
Sumanta Banerjee <sumanta.banerjeein@gmail.com>
2020-02-24 12:40:52 +0530

    Call back modificatoin done and complited

--- hcesdk/src/main/java/com/conduent/hcesdk/RetrieveRemoteOfferCallback.java
+++ hcesdk/src/main/java/com/conduent/hcesdk/RetrieveRemoteOfferCallback.java
@@ -1,17 +1,6 @@
 package com.conduent.hcesdk;
 
 public interface RetrieveRemoteOfferCallback {
-//    void onRetrieveRemoteOffer();
-//
-//    void onRetrieveRemoteOfferError(Failure error);
-
-//    void onStarted();
-//
-//    void onEnded();
-//
-//    void onError(Failure error, String message);
-//
-//    void onTimeOut();
 
     void onStarted();
 
